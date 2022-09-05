e14b18f53d33696bc450156ee8f822e947d0909a
123iklimah123 <iklimahika57@gmail.com>
2022-09-05 13:52:05 +0700

    first commit

--- novice/03-05/main.py
+++ novice/03-05/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 app = Flask(__name__)
 
@@ -8,7 +8,8 @@ def index():
             host="localhost",
             database="contoh",  
             user="postgres",
-            password="allah1")
+            password="allah1"
+    )
 
     curs = conn.cursor()
     if request.method == "POST":
@@ -17,9 +18,7 @@ def index():
         query = f"insert into buah(nama, detail) values ('{nama}','{detail}')"
         curs.execute(query)
         conn.commit()
-        curs.close()
-        conn.close()
-
+       
         # print(20*"=")
         # print(nama)
         # print(detail)
@@ -29,10 +28,10 @@ def index():
     print(request.method)
     query = f"select * from buah"
     curs.execute(query)
-    data = curs.fetchone()
+    data = curs.fetchall()
     curs.close()
     conn.close()
-    data = ["apel", "pear", "anggur", "jeruk", "belimbing", "kelengkeng"]
+    # data = ["apel", "pear", "anggur", "jeruk", "belimbing", "kelengkeng"]
     return render_template("index.html", context=data)
 
 @app.route("/detail/<buah_id>")
@@ -46,15 +45,14 @@ def detail(buah_id):
     curs = conn.cursor()
     query = f"select * from buah where id = {buah_id}"
     curs.execute(query)
-    data = curs.fechthone()
-    conn.commit()
+    data = curs.fetchone()
     curs.close()
     conn.close()
     print(data)
     return render_template("detail.html", context=data)
 
-    @app.route("/detail/<buah_id>")
-def detail(buah_id):
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
     conn = psycopg2.connect(
             host="localhost",
             database="contoh",  
@@ -62,12 +60,11 @@ def detail(buah_id):
             password="allah1"
 )
     curs = conn.cursor()
-    query = f"select * from buah where id = {buah_id}"
+    query = f"delete from buah where id = {buah_id}"
     curs.execute(query)
     conn.commit()
     curs.close()
     conn.close()
-    print(data)
     return redirect("/")
 
 if __name__ == "__main__":
