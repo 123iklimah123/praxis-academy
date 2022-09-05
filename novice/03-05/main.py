da426e1f9d4bad13089f9ecd50e1a3e1d379189f
123iklimah123 <iklimahika57@gmail.com>
2022-09-05 11:14:56 +0700

    first commit

--- novice/03-05/main.py
+++ novice/03-05/main.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
-import psycopg2 
-
+import psycopg2
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     conn = psycopg2.connect(
             host="localhost",
@@ -15,19 +14,61 @@ def index():
     if request.method == "POST":
         nama = request.form.get("nama")
         detail = request.form.get("detail")
-        query = f"insert into buah(nama, detail) values('{nama}','{detail}')"
+        query = f"insert into buah(nama, detail) values ('{nama}','{detail}')"
         curs.execute(query)
         conn.commit()
         curs.close()
         conn.close()
 
+        # print(20*"=")
+        # print(nama)
+        # print(detail)
+        # print(20*"=")
     
     
     print(request.method)
-
-    data = ["apel", "pear", "anggur"]
+    query = f"select * from buah"
+    curs.execute(query)
+    data = curs.fetchone()
+    curs.close()
+    conn.close()
+    data = ["apel", "pear", "anggur", "jeruk", "belimbing", "kelengkeng"]
     return render_template("index.html", context=data)
 
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="contoh",  
+            user="postgres",
+            password="allah1"
+)
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fechthone()
+    conn.commit()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
+    @app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="contoh",  
+            user="postgres",
+            password="allah1"
+)
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    print(data)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
